a78c0856793e46563641fef3c5138994c559132c
ikz87 <iikz87ii@gmail.com>
2024-08-09 16:15:02 -0600

    Add descriptions for the uniforms

--- template.glsl
+++ template.glsl
@@ -1,14 +1,13 @@
 #version 330 core
 out vec4 FragColor;
 
-uniform vec3 iResolution;
-uniform float iTime;
-uniform float iTimeDelta;
-uniform float iBatteryLevel;
-uniform float iLocalTime;
-uniform vec4 iMouse;
-uniform sampler2D myTexture;
-
+uniform vec3 iResolution; // Resolution of the shader window
+uniform float iTime; // Time in seconds since start of rendering
+uniform float iTimeDelta; // Time in seconds since last frame
+uniform float iBatteryLevel; // Battery level of your pc, from 0 (empty) to 1 (full)
+uniform float iLocalTime; // Local time of your pc, from 0 (12 am) to 1 (11:59:59 pm)
+uniform vec4 iMouse; // Position of your mouse (z and w components unused as of now)
+uniform sampler2D myTexture; // Texture extracted from a jpeg file
 
 // Paste shadertoy code here, as long as the shader doesn't
 // use any channels or buffers, it should work
